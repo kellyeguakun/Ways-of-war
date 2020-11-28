d8c04afc5228df636ecb74f25476c8c46bc5f0a7
kellyeguakun <kellyeguakun@gmail.com>
2020-11-28 15:30:28 +0000

    latest project changes

---
+++ WaysOfWar(Source Code)/Assets/Scripts/OpenLink.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenChannel()
+    {
+        Application.OpenURL("https://github.com/kellyeguakun/Ways-of-war");
+    }
+   
+}
